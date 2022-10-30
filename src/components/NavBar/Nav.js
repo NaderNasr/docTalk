e0d328b5ac730cb91031e859157bbcfe8d26a362
NaderNasr <nadernasr7@gmail.com>
2022-10-30 13:38:12 -0400

    fixes

--- src/components/NavBar/Nav.js
+++ src/components/NavBar/Nav.js
@@ -5,24 +5,30 @@ function Nav({ isLocal, pokeDetails, setIsLocal }) {
   return (
     <>
       <div className='container'>
-        <Navbar>
-          <Container>
-            <Navbar.Brand><strong><h1>Poke Dex</h1></strong></Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text className='center'>
-                <p>Rendering From: <strong>{isLocal ? 'Express DB' : 'Poke API'}</strong></p>
+        <Container>
+          <Navbar>
+            <Container>
+              <Navbar.Brand>
+                <strong>
+                  <h1 style={{lineHeight:'25px'}}>PokeDex</h1>
+                </strong>
+                <p style={{ fontSize: '10px', lineHeight:'1px', marginLeft:'2px' }}>by: Nader Nasr</p>
+              </Navbar.Brand>
+              <Navbar.Collapse />
+              <div className='center'>
+                <Navbar.Text>
+                  <p>Rendering From: <strong>{isLocal ? 'Express DB' : 'Poke API'}</strong></p>
+                </Navbar.Text>
                 <p>Number of cards <strong>{pokeDetails.length}</strong> </p>
-              </Navbar.Text>
-              <Navbar.Text>
-                <button onClick={() => setIsLocal(!isLocal)} style={{ marginBottom: '20px' }}>
-                  Get {!isLocal ? 'Express DB' : 'Poke API'}
-                </button>
-              </Navbar.Text>
-
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+                <Navbar.Text>
+                </Navbar.Text>
+              </div>
+              <button onClick={() => setIsLocal(!isLocal)} style={{ marginBottom: '20px' }}>
+                Get {!isLocal ? 'Express DB' : 'Poke API'}
+              </button>
+            </Container>
+          </Navbar>
+        </Container>
       </div>
     </>
   );
