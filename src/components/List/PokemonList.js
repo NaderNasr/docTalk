3331df1e1f3dd0571f00bbfa7a5983422ccb0880
NaderNasr <nadernasr7@gmail.com>
2022-10-30 22:52:23 -0400

    fixed errors

--- src/components/List/PokemonList.js
+++ src/components/List/PokemonList.js
@@ -3,20 +3,18 @@ import React from 'react';
 const PokemonList = ({ poke, isLocal, savePoke }) => {
 
   const uppercaseWords = str => str.replace(/^(.)|\s+(.)/g, firstLetter => firstLetter.toUpperCase());
-
-  const id = poke?.name
   return (
     <div className="app-contaner">
       <table>
         <tbody>
-          <tr key={id}>
+          <tr>
             <td><img className='detail-image' src={poke?.img} alt='pokemon' /></td>
             <td>{uppercaseWords(poke?.name)}</td>
             <td>
-              {poke?.types?.map((type, id) => (
-                <>
-                  <span key={id}>{type}</span> <br /><br />
-                </>
+              {poke?.types?.forEach((type, id) => (
+                <div>
+                  <div style={{marginbottom:'10px'}}>{type}</div>
+                </div>
               ))}
             </td>
           </tr>
